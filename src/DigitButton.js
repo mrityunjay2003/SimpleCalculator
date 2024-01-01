c1b683bb58b23b6eb825b5c9cb95a5c5d566d038
mrityunjay2003 <mrityunjay281203@gmail.com>
2024-01-01 20:46:03 +0530

    Initial Commit

---
+++ src/DigitButton.js
@@ -0,0 +1,9 @@
+import { ACTIONS } from "./App"
+
+export function DigitButton({dispatch,digit}){
+    return (
+    <button onClick={()=>dispatch({type:ACTIONS.ADD_DIGIT,payload:{digit}})}>
+    {digit}
+    </button>
+    )
+}
